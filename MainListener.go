cb686cd3b11d7b34a2fb29cd6d3e776d7d9a25cc
colbyktang <colby.us2012@gmail.com>
2020-11-19 14:24:39 -0600

    adding go listener

---
+++ MainListener.go
@@ -0,0 +1,17 @@
+package PersonalWebServer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":80", nil)
+	fmt.Printf("hello, world\n")
+}
